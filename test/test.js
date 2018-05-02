ee2bbf0aef37a5ea010e5502aa6fb153818683cd
ChristopheCIATTI <37122204+ChristopheCIATTI@users.noreply.github.com>
2018-05-02 16:27:14 +0200

    Javascript test file

--- test/test.js
+++ test/test.js
@@ -1,33 +1,41 @@
-var popUp = require("../../index.js");
+// File of test javascript with mocha & chai
 
+// require the module which you test
+var popUp = require("../lib/popUp/index.js");
+
+// require the test library javascript
 var mocha = require("mocha");
 var chai = require("chai");
 
+// require DOM
 var document = require("jsdom").jsdom(
 	"<!doctype html><html><head></head><body></body></html>");
 var window = document.defaultView;
 window.document = document;
 
+// init the module
 popUp.init(window);
-mocha.describe("PopUp", function() {
-	mocha.it("PopUp.js is a module ?", function() {
-		chai.assert("object" === typeof popUp);
-	});
 
-	mocha.describe("Methods", function() {
-		popUp.popUpDisplay("foo");
-		// check see you soon
+// below the tests
+// describe and check if poUp.js is a module
+// to check use chai.assert and check if popUp is a object
+mocha.describe("popUp", function() {
+    mocha.it("popUp.js is a module ?", function() {
+        chai.assert("object" === typeof popUp);
+    });
 
-		chai.assert(
-			"object" === typeof window.document.body.lastChild &&
-			"popUp" === window.document.body.lastChild.getAttribute("id") &&
-			"DIV" === window.document.body.lastChild.tagName
-		);
-		window.document.body.removeChild(
-			window.document.body.lastChild
-		);
-	});
-	mocha.it("popUpDisplay is a function ?", function() {
-		chai.assert("function" === typeof popUp.popUpDisplay);
+    // describe the test
+    mocha.describe("Methods", function () {
+    	// write test here
+    	
+    	//check if popUp.popUpDisplay run 
+        mocha.it("Does popUpDisplay create popUp ?", function(){
+            popUp.popUpDisplay("foo");
+        });
+
+        // Check is popUpDisplay is a function
+        mocha.it("popUpDisplay is a function ?", function(){
+     		chai.assert("function" === typeof popUp.popUpDisplay);
+    	});
 	});
 });

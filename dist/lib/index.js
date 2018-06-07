e335745b2ec2104532ba9de803a7979df9ca1f8f
ChristopheCIATTI <37122204+ChristopheCIATTI@users.noreply.github.com>
2018-06-07 16:35:24 +0200

    Add files via upload

--- dist/lib/index.js
+++ dist/lib/index.js
@@ -1,115 +1,133 @@
-"use strict";
-
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
-
-(function (root, factory) {
-	if ((typeof module === "undefined" ? "undefined" : _typeof(module)) === "object" && module.exports) {
-		module.exports = factory();
+(function (global, factory) {
+	if (typeof define === "function" && define.amd) {
+		define(["module"], factory);
+	} else if (typeof exports !== "undefined") {
+		factory(module);
 	} else {
-		root.popUp = factory(root);
+		var mod = {
+			exports: {}
+		};
+		factory(mod);
+		global.index = mod.exports;
 	}
-}(this, function (root) {
+})(this, function (module) {
+	"use strict";
 
-	if (root) {
-		var style = root.document.createElement("style");
-		style.type = "text/css";
+	var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) {
+		return typeof obj;
+	} : function (obj) {
+		return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
+	};
 
-		var css = root.document.createTextNode("div {}");
-
-		if (style.styleSheet) {
-			style.styleSheet.cssText = css;
+	(function (root, factory) {
+		if ((typeof module === "undefined" ? "undefined" : _typeof(module)) === "object" && module.exports) {
+			module.exports = factory();
 		} else {
-			style.appendChild(css);
+			root.popUp = factory(root);
 		}
-		root.document.getElementsByTagName("head")[0].appendChild(style);
-	}
+	})(undefined, function (root) {
 
-	var getMessageFunction = function getMessageFunction(message) {
-		if (typeof message !== "string") {
-			throw new Error("Message must be string");
-		}
+		if (root) {
+			var style = root.document.createElement("style");
+			style.type = "text/css";
 
-		var div = root.document.createElement("div");
-		var p = root.document.createElement("p");
-		var aClose = root.document.createElement("a");
-		var buttonConfirm = root.document.createElement("button");
-		var buttonCancel = root.document.createElement("button");
+			var css = root.document.createTextNode("div {}");
 
-		var pText = root.document.createTextNode(message);
-		aClose.innerHTML = "&#x274c;";
-		var buttonConfirmText = root.document.createTextNode("I visit");
-		var buttonCancelText = root.document.createTextNode("Cancel");
-
-		p.appendChild(pText);
-		buttonConfirm.appendChild(buttonConfirmText);
-		buttonCancel.appendChild(buttonCancelText);
-
-		div.appendChild(aClose);
-		div.appendChild(p);
-		div.appendChild(buttonConfirm);
-		div.appendChild(buttonCancel);
-		div.setAttribute("id", "PopUpStyle");
-		div.style.boxShadow = "0 75px 100px #black";
-
-		aClose.onclick = function () {
-			var parent = aClose.parentNode;
-			parent.style.left = "0%";
-			buttonConfirm.style.background = "#C60800";
-			buttonCancel.style.background = "#C60800";
-			parent.style.background = "#C60800";
-			parent.style.opacity = 0;
-			window.setTimeout(function () {
-				parent.parentNode.removeChild(parent);
-			}, 1000);
-		};
-
-		buttonConfirm.onclick = function () {
-			buttonConfirm.onclick = null;
-			var parent = buttonConfirm.parentNode;
-			buttonConfirm.style.background = "green";
-			buttonCancel.style.background = "green";
-			parent.style.background = "green";
-			parent.style.opacity = 0;
-			window.setTimeout(function () {
-				parent.parentNode.removeChild(parent);
-			}, 1000);
-		};
-
-		buttonCancel.onclick = function () {
-			var parent = buttonCancel.parentNode;
-			parent.style.left = "100%";
-			buttonCancel.style.background = "yellow";
-			buttonConfirm.style.background = "yellow";
-			parent.style.background = "yellow";
-			parent.style.opacity = 0;
-			window.setTimeout(function () {
-				parent.parentNode.removeChild(parent);
-			}, 1000);
-		};
-		return div;
-	};
-
-	var log = function log(messageFunction) {
-		if (!(messageFunction instanceof root.HTMLElement) || !root.document.body) {
-			throw new Error("Require a valid document an a body");
-		}
-		root.document.body.appendChild(messageFunction);
-		messageFunction.clientHeight;
-		messageFunction.style.left = "20%";
-	};
-
-	return function () {
-		var self = {};
-		self.init = function (window) {
-			if (!window || !window.document) {
-				throw new Error("popUp requires a window with a document");
+			if (style.styleSheet) {
+				style.styleSheet.cssText = css;
+			} else {
+				style.appendChild(css);
 			}
-			root = window;
+			root.document.getElementsByTagName("head")[0].appendChild(style);
+		}
+
+		var getMessageFunction = function getMessageFunction(message) {
+			if (typeof message !== "string") {
+				throw new Error("Message must be string");
+			}
+
+			var div = root.document.createElement("div");
+			var p = root.document.createElement("p");
+			var aClose = root.document.createElement("a");
+			var buttonConfirm = root.document.createElement("button");
+			var buttonCancel = root.document.createElement("button");
+
+			var pText = root.document.createTextNode(message);
+			aClose.innerHTML = "&#x274c;";
+			var buttonConfirmText = root.document.createTextNode("I visit");
+			var buttonCancelText = root.document.createTextNode("Cancel");
+
+			p.appendChild(pText);
+			buttonConfirm.appendChild(buttonConfirmText);
+			buttonCancel.appendChild(buttonCancelText);
+
+			div.appendChild(aClose);
+			div.appendChild(p);
+			div.appendChild(buttonConfirm);
+			div.appendChild(buttonCancel);
+			div.setAttribute("id", "PopUpStyle");
+			div.style.boxShadow = "0 75px 100px #black";
+
+			aClose.onclick = function () {
+				var parent = aClose.parentNode;
+				parent.style.left = "0%";
+				buttonConfirm.style.background = "#C60800";
+				buttonCancel.style.background = "#C60800";
+				parent.style.background = "#C60800";
+				parent.style.opacity = 0;
+				window.setTimeout(function () {
+					parent.parentNode.removeChild(parent);
+				}, 1000);
+			};
+
+			buttonConfirm.onclick = function () {
+				buttonConfirm.onclick = null;
+				var parent = buttonConfirm.parentNode;
+				buttonConfirm.style.background = "green";
+				buttonCancel.style.background = "green";
+				parent.style.background = "green";
+				parent.style.opacity = 0;
+				window.setTimeout(function () {
+					parent.parentNode.removeChild(parent);
+				}, 1000);
+			};
+
+			buttonCancel.onclick = function () {
+				var parent = buttonCancel.parentNode;
+				parent.style.left = "100%";
+				buttonCancel.style.background = "yellow";
+				buttonConfirm.style.background = "yellow";
+				parent.style.background = "yellow";
+				parent.style.opacity = 0;
+				window.setTimeout(function () {
+					parent.parentNode.removeChild(parent);
+				}, 1000);
+			};
+			return div;
 		};
-		self.popUpDisplay = function (message) {
-			var messageFunction = getMessageFunction(message);
-			log(messageFunction);
+
+		var log = function log(messageFunction) {
+			if (!(messageFunction instanceof root.HTMLElement) || !root.document.body) {
+				throw new Error("Require a valid document an a body");
+			}
+			root.document.body.appendChild(messageFunction);
+			messageFunction.clientHeight;
+			messageFunction.style.left = "20%";
 		};
-		return self;
-	}();
-}));
+
+		return function () {
+			var self = {};
+			self.init = function (window) {
+				if (!window || !window.document) {
+					throw new Error("popUp requires a window with a document");
+				}
+				root = window;
+			};
+			self.popUpDisplay = function (message) {
+				var messageFunction = getMessageFunction(message);
+				log(messageFunction);
+			};
+			return self;
+		}();
+	});
+});
